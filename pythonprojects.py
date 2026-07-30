60c842e84566e45151c8b6f544c5df960275ea4f
aastha sharma <asthasharma1012@gmail.com>
2026-07-30 11:23:58 +0530

    scerect langauge project

--- pythonprojects.py
+++ pythonprojects.py
@@ -1422,22 +1422,52 @@
     # raise IndexError("value should be under l index range")
 
 
-class MinorError(Exception):
-    pass
+# class MinorError(Exception):
+#     pass
 
-def voting_eligiblity(age):
-    if age<18:
-        raise MinorError("age must be greater than 18")
-    else:
-        print("eligible for voting")
+# def voting_eligiblity(age):
+#     if age<18:
+#         raise MinorError("age must be greater than 18")
+#     else:
+#         print("eligible for voting")
        
 
-try:
-    age = int(input("enter your age: "))
-    voting_eligiblity(age)
-except MinorError as error:
-    print(f"error caught: {error}")
-except ValueError:      
-    print("enter valid age")     
+# try:
+#     age = int(input("enter your age: "))
+#     voting_eligiblity(age)
+# except MinorError as error:
+#     print(f"error caught: {error}")
+# except ValueError:      
+#     print("enter valid age")     
 
+import random
+import string
 
+print("----secret language coding and decoding----")
+user_message = input("enter your message: ")
+user_choice = input("encode or decode?: ").strip().lower()
+
+words = user_message.split()
+new_words = []
+if user_choice == "encode":
+    for word in words:
+        if len(word) >= 3:
+            r1 = "".join(random.choices(string.ascii_lowercase, k=3))
+            r2 = "".join(random.choices(string.ascii_lowercase, k=3))
+            encoded_word = r1 + word[1:] + word[0] + r2
+            new_words.append(encoded_word)
+        else:
+            new_words.append(word[::-1])
+    result = " ".join(new_words)
+    print(f"encoded word : {result} ")
+
+elif user_choice == "decode":
+    for word in words:
+        if len(word) >= 6:
+            stripped_word = word[3:-3]
+            decoded_word = stripped_word[-1] + stripped_word[:-1]
+            new_words.append(decoded_word)
+        else:
+            new_words.append(word[::-1])
+    result = " ".join(new_words)
+    print(f"decoded word: {result}")
---
+++ secret_language.py
@@ -0,0 +1,31 @@
+import random
+import string
+
+print("----secret language coding and decoding----")
+user_message = input("enter your message: ")
+user_choice = input("encode or decode?: ").strip().lower()
+
+words = user_message.split()
+new_words = []
+if user_choice == "encode":
+    for word in words:
+        if len(word) >= 3:
+            r1 = "".join(random.choices(string.ascii_lowercase, k=3))
+            r2 = "".join(random.choices(string.ascii_lowercase, k=3))
+            encoded_word = r1 + word[1:] + word[0] + r2
+            new_words.append(encoded_word)
+        else:
+            new_words.append(word[::-1])
+    result = " ".join(new_words)
+    print(f"encoded word : {result} ")
+
+elif user_choice == "decode":
+    for word in words:
+        if len(word) >= 6:
+            stripped_word = word[3:-3]
+            decoded_word = stripped_word[-1] + stripped_word[:-1]
+            new_words.append(decoded_word)
+        else:
+            new_words.append(word[::-1])
+    result = " ".join(new_words)
+    print(f"decoded word: {result}")

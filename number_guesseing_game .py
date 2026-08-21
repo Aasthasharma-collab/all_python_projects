4ad197b45010448390e6ec3f3c9785ea545ef97b
aastha sharma <asthasharma1012@gmail.com>
2026-08-21 14:42:39 +0530

    Add 5-number guessing game logic

--- number_guesseing_game .py
+++ number_guesseing_game .py
@@ -16,4 +16,31 @@ if user_input != secret_number:
         else:
             print("Too high! Try guessing a lower number.")
     else:
-        print(f"Sorry, you've used all attempts. The correct number was {secret_number}.")    
+        print(f"Sorry, you've used all attempts. The correct number was {secret_number}.")
+
+import random
+print()
+print("----random number guessing game----\n")
+print("I have selected 5 random numbers between 1 and 20. Try to guess one of them!")
+number = [random.randint(1, 20) for _ in range(5)]
+for attempt in range(1, 11):
+    user_input = int(input(f"Attempt {attempt}: Enter your guess (1-20): "))
+
+    if user_input in number:
+        number.remove(user_input)
+        print(
+            f"Congratulations! You guessed {user_input}. Remaining numbers to"
+            f" guess: {len(number)}\n"
+        )
+        if len(number) == 0:
+            print("You won! You found all 5 numbers!")
+            break
+    elif user_input < min(number):
+        print("Too low! Try guessing a higher number.")
+    elif user_input > max(number):
+        print("Too high! Try guessing a lower number.")
+    else:
+        print("Incorrect guess! Try again.")    
+else:
+    print("you have run out of attempts. The correct numbers were: ", number)
+    print("Game Over!")

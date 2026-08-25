3683f243c84f43fe4941477ff296334016ff1fa8
aastha sharma <asthasharma1012@gmail.com>
2026-08-25 10:32:01 +0530

    Add greet deorator for bank transactions

--- AS_bank.py
+++ AS_bank.py
@@ -3,11 +3,13 @@ class account:
         self.account_number = account_number
         self.balance = balance
 
+    def greet(self):
+        print("welcome to the AS BANK 🙏🏻")    
+
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
             print()
-            print("welcome to the AS BANK 🙏🏻")
             print(f"Deposited: {amount}. New balance: {self.balance}\n")
         else:
             print("Deposit amount must be positive.")
@@ -16,18 +18,16 @@ class account:
         if 0 < amount <= self.balance:
             self.balance -= amount
             print()
-            print("welcome to the AS BANK 🙏🏻")
             print(f"Withdrew: {amount}. New balance: {self.balance}\n")
         else:
             print("Invalid withdrawal amount.")
 
     def display_balance(self):
         print()
-        print("welcome to the AS BANK 🙏🏻")
         print(f"for Account Number: {self.account_number}, Balance: {self.balance}\n")
 
-a = account("123456789", 1000)
+a = account("123456789", 2000)
+a.greet()
 a.display_balance()
 a.deposit(500)
 a.withdraw(200)
-a.display_balance()
